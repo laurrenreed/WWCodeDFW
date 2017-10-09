7cb732063fbfae9c050f6e3d5554ffec829592da
IOSDEV-44490 <alkalocal@alk-mac0176.lan>
2017-10-09 10:16:43 -0500

    Fix swift linting file, update main vc with hello world

---
+++ WWCodeDFW/WWCodeDFW/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  WWCodeDFW
+//
+//  Created by AlkaLocal on 10/9/17.
+//  Copyright © 2017 WWCode. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import SnapKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel.init()
+        label.text = "Hello, world!"
+        self.view.addSubview(label)
+        label.snp.makeConstraints { (make) in
+            make.centerY.centerX.equalTo(self.view)
+            make.height.width.equalTo(100)
+        }
+        
+    }
+}
