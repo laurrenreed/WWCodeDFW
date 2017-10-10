ec00555de1908543f7c8edf67c4e4752b8e1e6fe
Andrew Grant <andrew.grant@mubaloo.com>
2017-10-10 16:49:37 +0100

    Added TabBar and assets

--- WWCodeDFW/WWCodeDFW/AppDelegate.swift
+++ WWCodeDFW/WWCodeDFW/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //set global tab bar tint color
+        UITabBar.appearance().tintColor = UIColor(red: 59/255.0, green: 100/255.0, blue: 100/255.0, alpha: 1.0)
+        
         return true
     }
 
